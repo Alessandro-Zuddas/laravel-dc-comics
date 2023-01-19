99e662de90752d912e6458d0e908f7dddea2e551
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-01-19 17:32:27 +0100

    Aggiunge stampa dinamica dati in table homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>DC Comics</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -19,8 +19,41 @@
 
     <main>
 
-        <h1>Home Page</h1>
-        
+        <div class="container-fluid">
+            <h1 class="text-center">DC Comics Archive</h1>
+
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Thumb</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Series</th>
+                    <th scope="col">Sale date</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comics as $comic)
+                        <tr>
+                            <th scope="row">{{ $comic->id }}</th>
+                            <td>{{ $comic->title }}</td>
+                            <td>{{ $comic->description }}</td>
+                            <td>{{ $comic->thumb }}</td>
+                            <td>{{ $comic->price }}</td>
+                            <td>{{ $comic->series }}</td>
+                            <td>{{ $comic->sale_date }}</td>
+                            <td>{{ $comic->type }}</td>
+                            <td>Actions</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+              </table>
+        </div>
+
     </main>
 
 </body>
