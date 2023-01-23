55c17d7a6fb011757e88894ea2952d4a63219bad
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-01-23 10:53:56 +0100

    Cambia metodo store controller con fill

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -42,12 +42,7 @@ class ComicController extends Controller
 
         $new_comic = new Comic();
 
-        $new_comic->title = $data["title"];
-        $new_comic->description = $data["description"];
-        $new_comic->price = $data["price"];
-        $new_comic->series = $data["series"];
-        $new_comic->sale_date = $data["sale_date"];
-        $new_comic->type = $data["type"];
+        $new_comic->fill($data);
 
         $new_comic->save();
 
